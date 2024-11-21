5d493a305ec79466fca0c2bb44ce2a0004a7856f
Kirill <opoxmeloid@yandex.ru>
2024-11-21 18:24:15 +0200

    Better dev script

--- dev.ts
+++ dev.ts
@@ -1,6 +1,8 @@
 import { $ } from 'bun'
 
 await Promise.all([
+  $`cd packages/most-js-client && bun run generate-runtime`,
+  $`cd packages/most-js-client && bun run generate-types`,
   $`cd packages/most-js-client && bun run dev`,
   $`cd playground/vite-html && bun run dev`,
 ])
