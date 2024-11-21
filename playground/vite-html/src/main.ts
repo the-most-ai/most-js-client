5d8e1b9a33860503545e03073a6fc3fee0569aae
Kirill <opoxmeloid@yandex.ru>
2024-11-21 22:05:33 +0200

    extended example

--- playground/vite-html/src/main.ts
+++ playground/vite-html/src/main.ts
@@ -11,5 +11,11 @@ export async function doWorkWithMostAi(file: File) {
   const models = await client.listModels()
   console.log(`Models fetched:`, models.data)
   const audioUpload = await client.uploadAudio('67239029570a08554fc1f5a6', file)
-  console.log(`Audio uploaded`, audioUpload.)
+  console.log(`Audio uploaded`, audioUpload.data)
+  const applyModel = await client.applyModel(
+    '67239029570a08554fc1f5a6',
+    audioUpload.data.id,
+    models.data[0].model,
+  )
+  console.log(`Model applied`, applyModel.data)
 }
